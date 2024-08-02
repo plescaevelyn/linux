57ad7c6d8b4957d9689584d322e253be1cdd735a
Alexandru Andrei Alexa <alexaalexandru66@gmail.com>
2024-08-02 15:50:39 +0300

    ADC_EMU
    Signed-off-by: Alexandru Andrei Alexa <alexaalexandru66@gmail.com>

---
+++ drivers/iio/adc/iio-adc-emu.c
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright (c) 2024 Analog Devices
+ */
+
+#include <linux/spi/spi.h>
+#include <linux/module.h>
+
+#include <linux/iio/iio.h>
+
+static const struct iio_info iio_adc_emu_info = {
+
+}; 
+
+static int iio_adc_emu_probe(struct spi_device *spi)
+{
+    struct iio_dev *indio_dev;
+    int ret;
+
+    indio_dev = devm_iio_device_alloc(&spi->dev, 0);
+    if(!indio_dev)
+        return -ENOMEM;
+    
+    indio_dev->name = "iio-adc-emu";
+    indio_dev->info = &iio_adc_emu_info;
+
+    return devm_iio_device_register(&spi->dev, indio_dev);
+}
+
+static struct spi_driver iio_adc_emu_driver = {
+    .driver = {
+        .name = "iio-adc-emu",
+    },
+    .probe = iio_adc_emu_probe
+};
+
+module_spi_driver(iio_adc_emu_driver);
+
+
+MODULE_AUTHOR("Alexa Alexandru");
+MODULE_DESCRIPTION("Analog Device IIO EMU ADC Driver");
+MODULE_LICENSE("GPL v2");
