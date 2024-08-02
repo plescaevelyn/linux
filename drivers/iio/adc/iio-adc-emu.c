9076230393a5e9691ba7363432bc815b297e9392
Wally4Ever <andrei_valentyn_2008@yahoo.com>
2024-08-02 15:57:35 +0300

    EMU working, v1.0

---
+++ drivers/iio/adc/iio-adc-emu.c
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Driver for AD5592R
+ *
+ * Copyright (C) 2024 Analog Devices, Inc.
+ */
+#include <linux/spi/spi.h>
+#include <linux/module.h>
+#include <linux/iio/iio.h>
+
+
+static const struct iio_info iio_adc_emu_info = {
+
+};
+
+static int iio_adc_emu_probe ( struct spi_device *spi ) {
+
+    struct iio_dev *indio_dev;
+    //int ret;
+
+    
+    indio_dev = devm_iio_device_alloc(&spi -> dev, 0);
+
+    if ( !indio_dev ) {
+        return -ENOMEM;
+    }
+;
+    indio_dev -> name = "iio-adc-emu";
+    indio_dev -> info = &iio_adc_emu_info;
+
+    return devm_iio_device_register(&spi->dev, indio_dev);
+ 
+}
+
+static struct spi_driver iio_adc_emu_driver = {
+
+    .driver = { .name = "iio-adc-emu" },
+
+    .probe = iio_adc_emu_probe
+};
+
+module_spi_driver(iio_adc_emu_driver);
+
+MODULE_AUTHOR ("Valentin Ples valentin.ples02@gmai.com");
+MODULE_DESCRIPTION ("Analog Devices IIO ADC");
+MODULE_LICENSE ("GPL V2");
+
