de22026f5950f04567b3519b59fcb2ed70464fd4
Wally4Ever <andrei_valentyn_2008@yahoo.com>
2024-08-02 18:22:43 +0300

    ADC working, v1.0

---
+++ drivers/iio/adc/ad5592rs.c
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: GPL-2.0
+//Copyright (C) 2024 Analog Devices, Inc.
+#include <linux/spi/spi.h>
+#include <linux/module.h>
+#include <linux/iio/iio.h>
+
+
+static const struct iio_info ad5592rs_info = {
+
+};
+
+static int ad5592rs_probe ( struct spi_device *spi ) {
+
+    struct iio_dev *indio_dev;
+    //int ret;
+
+    
+    indio_dev = devm_iio_device_alloc(&spi -> dev, 0);
+
+    if ( !indio_dev ) {
+        return -ENOMEM;
+    }
+;
+    indio_dev -> name = "ad5592rs";
+    indio_dev -> info = &ad5592rs_info;
+
+    return devm_iio_device_register(&spi->dev, indio_dev);
+ 
+}
+
+static struct spi_driver ad5592rs_driver = {
+
+    .driver = { .name = "ad5592rs" },
+
+    .probe = ad5592rs_probe
+};
+
+module_spi_driver(ad5592rs_driver);
+
+MODULE_AUTHOR ("Valentin Ples valentin.ples02@gmai.com");
+MODULE_DESCRIPTION ("Analog Devices IIO ADC");
+MODULE_LICENSE ("GPL V2");
+
