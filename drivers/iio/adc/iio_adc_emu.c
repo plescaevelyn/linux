8f2b5a2ac9176dcacb26f6658568a5464cd6c0fd
Raluca Zaicanu <ralucazaicanu@yahoo.ro>
2024-08-05 11:23:23 +0300

    emu-luni
    Signed-off-by: Raluca Zaicanu <ralucazaicanu@yahoo.ro>

--- drivers/iio/adc/iio_adc_emu.c
+++ drivers/iio/adc/iio_adc_emu.c
@@ -6,9 +6,43 @@
 #include <linux/spi/spi.h>
 #include <linux/module.h>
 #include <linux/iio/iio.h>
+struct iio_chan_spec const iio_adc_emu_chans[] = {
+    {
+        .type = IIO_VOLTAGE,
+        .indexed = 1,
+        .channel = 0,
+        .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+    },
+    {
+        .type = IIO_VOLTAGE,
+        .indexed = 1,
+        .channel = 1,
+        .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+    }
+};
+static int iio_adc_emu_read_raw(struct iio_dev *indio_dev,
+			struct iio_chan_spec const *chan,
+			int *val,
+			int *val2,
+			long mask)
+{
+    switch (mask)
+    {
+    case IIO_CHAN_INFO_RAW:
+        if(chan->channel)
+            *val =  87;
+        else
+            *val = 420;
+        return IIO_VAL_INT;
+    default:
+        return -EINVAL;
+    }
+
+    return -EINVAL;
+}
 
 static const struct iio_info iio_adc_emu_info = {
-
+    .read_raw = &iio_adc_emu_read_raw,
 };
 
  static int iio_adc_emu_probe(struct spi_device *spi)
@@ -21,7 +55,8 @@ static const struct iio_info iio_adc_emu_info = {
         return -ENOMEM; //Error -> No memory
     indio_dev->name = "iio-adc-emu";
     indio_dev->info = &iio_adc_emu_info;
-    
+    indio_dev->channels = iio_adc_emu_chans;
+    indio_dev->num_channels = ARRAY_SIZE(iio_adc_emu_chans);
     return devm_iio_device_register(&spi->dev, indio_dev);
  }
 
@@ -29,11 +64,11 @@ static const struct iio_info iio_adc_emu_info = {
     .driver = {
         .name = "iio-adc-emu",
     },
-    .probe = &iio_adc_emu_probe
+    .probe = iio_adc_emu_probe
  };
 
  module_spi_driver(iio_adc_emu_driver);
 
 MODULE_AUTHOR("Risteiu Ioana");
 MODULE_DESCRIPTION("Analog Devices IIO ADC EMU");
-MODULE_LICENSE("GPL v2");
+MODULE_LICENSE("GPL v2");
