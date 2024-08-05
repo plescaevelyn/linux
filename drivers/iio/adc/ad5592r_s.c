2e56f9afcb57274f39bbb42f4da3e2a2063ae7d8
Pavel Robert <pavel.robert0110@gmail.com>
2024-08-05 14:28:13 +0300

    Channels for adc
    Signed-off-by: Pavel Robert <pavel.robert0110@gmail.com>

--- drivers/iio/adc/ad5592r_s.c
+++ drivers/iio/adc/ad5592r_s.c
@@ -7,8 +7,90 @@
 #include <linux/module.h>
 #include <linux/iio/iio.h>
 
-static const struct iio_info ad5592r_s_info = {
+static struct iio_chan_spec const iio_adc_chans[] = {
+    {
+        .type = IIO_VOLTAGE,
+        .indexed = 1,
+        .channel = 0,
+        .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+    },
 
+    {
+        .type = IIO_VOLTAGE,
+        .indexed = 1,
+        .channel = 1,
+        .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+    },
+
+    {
+        .type = IIO_VOLTAGE,
+        .indexed = 1,
+        .channel = 2,
+        .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+    },
+
+    {
+        .type = IIO_VOLTAGE,
+        .indexed = 1,
+        .channel = 3,
+        .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+    },
+
+    {
+        .type = IIO_VOLTAGE,
+        .indexed = 1,
+        .channel = 4,
+        .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+    },
+
+    {
+        .type = IIO_VOLTAGE,
+        .indexed = 1,
+        .channel = 5,
+        .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+    }
+};
+
+static int iio_adc_read_raw  (struct iio_dev *indio_dev,
+            struct iio_chan_spec const *chan,
+            int *val,
+            int *val2,
+            long mask)
+{
+    switch(mask) {
+    case IIO_CHAN_INFO_RAW: {
+        switch(chan->channel) {
+            case 0:
+                *val = 100;
+                break;
+            case 1:
+                *val = 200;
+                break;
+            case 2:
+                *val = 300;
+                break;
+            case 3:
+                *val = 400;
+                break;
+            case 4:
+                *val = 500;
+                break;
+            case 5:
+                *val = 600;
+                break;
+            default:
+                return -EINVAL;
+        }
+
+        return IIO_VAL_INT;
+    }
+    default:
+        return -EINVAL;
+    }
+};
+
+static const struct iio_info ad5592r_s_info = {
+    .read_raw = &iio_adc_read_raw,
 };
 
 static int ad5592r_s_probe(struct spi_device *spi)
@@ -23,6 +105,9 @@ static int ad5592r_s_probe(struct spi_device *spi)
 
     indio_dev->name = "ad5592r_s";
     indio_dev->info = &ad5592r_s_info;
+
+    indio_dev->channels = iio_adc_chans;
+    indio_dev->num_channels = ARRAY_SIZE(iio_adc_chans);
 
     return devm_iio_device_register(&spi->dev, indio_dev);
 }
