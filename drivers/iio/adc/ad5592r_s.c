c9ab505c6eb2ce24ddeb9c5c78431fc3d19d9185
Volcov Sabina <volcovsabina@gmail.com>
2024-08-05 14:39:39 +0300

    Add 6 data channels to the AD5592R driver
    Signed-off-by: Volcov Sabina <volcovsabina@gmail.com>

--- drivers/iio/adc/ad5592r_s.c
+++ drivers/iio/adc/ad5592r_s.c
@@ -7,10 +7,105 @@
 #include <linux/module.h>
 #include <linux/iio/iio.h>
 
+struct iio_chan_spec const ad5592r_s_chans[] = {
+    {
+        .type = IIO_VOLTAGE,
+        .indexed = 1,
+        .channel = 0,
+        .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+    },
+    {
+        .type = IIO_VOLTAGE,
+        .indexed = 1,
+        .channel = 1,
+        .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+    },
+    {
+        .type = IIO_VOLTAGE,
+        .indexed = 1,
+        .channel = 2,
+        .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+    },
+    {
+        .type = IIO_VOLTAGE,
+        .indexed = 1,
+        .channel = 3,
+        .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+    },
+    {
+        .type = IIO_VOLTAGE,
+        .indexed = 1,
+        .channel = 4,
+        .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+    },
+    {
+        .type = IIO_VOLTAGE,
+        .indexed = 1,
+        .channel = 5,
+        .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+    }
+    //8 canale in datasheet, dar se folosesc 6
+    /*
+    {
+        .type = IIO_VOLTAGE,
+        .indexed = 1,
+        .channel = 6,
+        .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+    },
+    {
+        .type = IIO_VOLTAGE,
+        .indexed = 1,
+        .channel = 7,
+        .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+    }
+    */
+};
+
 //static pentru ca sunt functii interne
+static int ad5592r_s_read_raw (struct iio_dev *indio_dev,
+	    struct iio_chan_spec const *chan,
+		int *val,
+		int *val2,
+		long mask) 
+{
+
+    switch(mask) {
+        case IIO_CHAN_INFO_RAW:
+            if(chan->channel == 1) {
+                //canal 1
+                *val = 87;
+            }
+            else if(chan->channel == 2) {
+                //canal 2
+                *val = 65;
+            }
+            else if(chan->channel == 3) {
+                //canal 3
+                *val = 32;
+            }
+            else if(chan->channel == 4) {
+                //canal 4
+                *val = 41;
+            }
+            else if(chan->channel == 5) {
+                //canal 5
+                *val = 28;
+            }
+            else {
+                //canal 0
+                *val = 420;
+            }
+            return IIO_VAL_INT;
+            
+        default:
+            return -EINVAL;
+    }
+
+    return -EINVAL;
+}
 
 static const struct iio_info ad5592r_s_info = {
-
+    .read_raw = &ad5592r_s_read_raw,
 };
 
 static int ad5592r_s_probe(struct spi_device *spi) {
@@ -27,6 +122,8 @@ static int ad5592r_s_probe(struct spi_device *spi) {
 
     indio_dev->name = "ad5592r_s";
     indio_dev->info = &ad5592r_s_info;
+    indio_dev->channels = ad5592r_s_chans;
+    indio_dev->num_channels = ARRAY_SIZE(ad5592r_s_chans);
 
     return devm_iio_device_register(&spi->dev, indio_dev);
 }
