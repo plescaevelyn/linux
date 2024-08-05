bb1a35aaaff0bceb9777502f67e7b9c0e2abf4d1
Rosca David-Sorin <davidsrosca@gmail.com>
2024-08-05 11:22:56 +0300

    Test citire ADC
    Signed-off-by: Rosca David-Sorin <davidsrosca@gmail.com>

--- drivers/iio/adc/iio-adc-emu.c
+++ drivers/iio/adc/iio-adc-emu.c
@@ -7,9 +7,42 @@
 #include<linux/spi/spi.h>
 #include<linux/module.h>
 #include<linux/iio/iio.h>
+struct iio_chan_spec const iio_adc_emu_chans[]={
+   { .type = IIO_VOLTAGE,
+      .indexed=1,
+      .channel =0,
+      .info_mask_separate=BIT(IIO_CHAN_INFO_RAW),
+   },
+    {
+      .type = IIO_VOLTAGE,
+      .indexed=1,
+      .channel =1,
+      .info_mask_separate=BIT(IIO_CHAN_INFO_RAW),
+   }
+};
+static	int iio_adc_emu_read_raw(struct iio_dev *indio_dev,
+			struct iio_chan_spec const *chan,
+			int *val,
+			int *val2,
+			long mask)
+            {
+             switch (mask)
+             {
+             case IIO_CHAN_INFO_RAW:
+                if(chan->channel)
+                    *val =87;
+                    else 
+                    *val = 420;
+                return IIO_VAL_INT;
+             
+             default:
+                return -EINVAL;
+             }   
+             return -EINVAL;
+            }
 
 static const struct iio_info iio_adc_emu_info={
-  
+   .read_raw=&iio_adc_emu_read_raw,
 };
 static int iio_adc_emu_probe(struct spi_device *spi)
 {
@@ -20,7 +53,10 @@ static int iio_adc_emu_probe(struct spi_device *spi)
          return -ENOMEM;
     indio_dev->name ="iio-adc-emu";
     indio_dev->info = &iio_adc_emu_info;
-    return devm_iio_device_register(&spi->dev, indio_dev);
+    indio_dev->channels=iio_adc_emu_chans;
+    indio_dev->num_channels= ARRAY_SIZE(iio_adc_emu_chans);
+
+     return devm_iio_device_register(&spi->dev, indio_dev);
 }
 static struct spi_driver iio_adc_emu_driver={
     .driver={
