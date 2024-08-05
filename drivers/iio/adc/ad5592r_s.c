0b0f077bf651c950857e5747c4211216edb0977b
hadarauelena24 <analog@DESKTOP-SCUQ0E3.>
2024-08-05 14:09:48 +0300

    Add channels
    Signed-off-by: hadarauelena24 <analog@DESKTOP-SCUQ0E3.>

--- drivers/iio/adc/ad5592r_s.c
+++ drivers/iio/adc/ad5592r_s.c
@@ -7,8 +7,44 @@
 #include <linux/module.h>
 #include <linux/iio/iio.h>
 
-static const struct iio_info ad5592r_s_info = {
+struct iio_chan_spec const ad5592r_s_chans[]={
+    {
+        .type = IIO_VOLTAGE,
+        .indexed = 1, //daca vreau sa numerotez canalele
+        .channel = 0, //numar canal
+        .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+    },
+    {
+        .type = IIO_VOLTAGE,
+        .indexed = 1, //daca vreau sa numerotez canalele
+        .channel = 1, //numar canal
+        .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+    }
+};
 
+static int ad5592r_s_read_raw(struct iio_dev *indio_dev,
+			struct iio_chan_spec const *chan,
+			int *val,
+			int *val2,
+			long mask)
+{
+    switch(mask){
+        case IIO_CHAN_INFO_RAW:
+            if(chan->channel)
+                *val=87;
+            else
+                *val=420;
+            return IIO_VAL_INT;
+        
+        default:
+            return -EINVAL;
+    }
+
+    return -EINVAL;  
+}
+
+static const struct iio_info ad5592r_s_info = {
+     .read_raw=&ad5592r_s_read_raw,
 };
 
 static int ad5592r_s_probe(struct spi_device *spi)
@@ -22,6 +58,8 @@ static int ad5592r_s_probe(struct spi_device *spi)
 
     indio_dev->name="ad5592r_s";
     indio_dev->info = &ad5592r_s_info;
+    indio_dev->channels = ad5592r_s_chans;
+    indio_dev->num_channels = ARRAY_SIZE(ad5592r_s_chans);
 
     return devm_iio_device_register(&spi->dev,indio_dev);
 };
