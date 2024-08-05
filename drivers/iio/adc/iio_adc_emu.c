d59de0e8b12d56049454442d0e8f377bf8c6cdfc
GhisaAlexandru15 <ghisaalexandru15@gmail.com>
2024-08-05 08:21:52 +0300

    Test citire ADC
    Signed-off-by: GhisaAlexandru15 <ghisaalexandru15@gmail.com>

--- drivers/iio/adc/iio_adc_emu.c
+++ drivers/iio/adc/iio_adc_emu.c
@@ -7,8 +7,45 @@
 #include <linux/module.h>
 #include <linux/iio/iio.h>
 
-static const struct iio_info iio_adc_emu_info = {
+struct iio_chan_spec const iio_adc_emu_chans[] = {
+    {
+        .type = IIO_VOLTAGE,
+        .indexed = 1,
+        .channel = 0,
+        .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+    },
 
+    {
+        .type = IIO_VOLTAGE,
+        .indexed = 1,
+        .channel = 1,
+        .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+    }
+};
+
+static int iio_adc_emu_read_raw(struct iio_dev *indio_dev,
+			struct iio_chan_spec const *chan,
+			int *val,
+			int *val2,
+			long mask)
+{
+    switch (mask)
+    {
+    case IIO_CHAN_INFO_RAW:
+        if(chan->channel)
+            *val = 87;
+        else
+            *val = 420;
+        return IIO_VAL_INT;    
+    default:
+        return -EINVAL;
+    }
+
+    return -EINVAL;
+}
+
+static const struct iio_info iio_adc_emu_info = {
+    .read_raw = &iio_adc_emu_read_raw,
 }; 
 
 static int iio_adc_emu_probe(struct spi_device *spi)
@@ -22,15 +59,17 @@ static int iio_adc_emu_probe(struct spi_device *spi)
 
     indio_dev->name = "iio-adc-emu";
     indio_dev->info = &iio_adc_emu_info;
+    indio_dev->channels = iio_adc_emu_chans;
+    indio_dev->num_channels = ARRAY_SIZE(iio_adc_emu_chans);
 
     return devm_iio_device_register(&spi->dev, indio_dev);
 }
 
 static struct spi_driver iio_adc_emu_driver = {
     .driver = {
-        .name = "iio-adc-emu"
+        .name = "iio-adc-emu",
     },
-    .probe = iio_adc_emu_probe
+    .probe = iio_adc_emu_probe,
 };
 
 module_spi_driver(iio_adc_emu_driver);
