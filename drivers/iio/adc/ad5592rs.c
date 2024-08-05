e289056cadcf04f56c808758a1293045eedc1221
Wally4Ever <andrei_valentyn_2008@yahoo.com>
2024-08-05 14:06:48 +0300

    Add 6 channels, v1.1

--- drivers/iio/adc/ad5592rs.c
+++ drivers/iio/adc/ad5592rs.c
@@ -5,9 +5,94 @@
 #include <linux/iio/iio.h>
 
 
-static const struct iio_info ad5592rs_info = {
+struct iio_chan_spec const iio_ad5592rs_chans[] = {
+    {
+        .type = IIO_VOLTAGE,
+        .indexed = 1,
+        .channel = 0,
+        .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+    },
+    {
+        .type = IIO_VOLTAGE,
+        .indexed = 1,
+        .channel = 1,
+        .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+    },
+    {
+        .type = IIO_VOLTAGE,
+        .indexed = 1,
+        .channel = 2,
+        .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+    },
+    {
+        .type = IIO_VOLTAGE,
+        .indexed = 1,
+        .channel = 3,
+        .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+    },
+    {
+        .type = IIO_VOLTAGE,
+        .indexed = 1,
+        .channel = 4,
+        .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+    },
+    {
+        .type = IIO_VOLTAGE,
+        .indexed = 1,
+        .channel = 5,
+        .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+    }
 
 };
+
+static int iio_ad5592rs_read_raw  (struct iio_dev *indio_dev,
+			struct iio_chan_spec const *chan,
+			int *val,
+			int *val2,
+			long mask)
+{
+    switch (mask)
+    {
+        case IIO_CHAN_INFO_RAW:
+        {
+            switch (chan -> channel)
+            {
+                case 0:
+                    *val=87;
+                     break;
+                case 1:
+                    *val=96;
+                     break;
+                case 2:
+                    *val=69;
+                     break;
+                case 3:
+                    *val=420;
+                     break;
+                case 4:
+                    *val=80085;
+                     break;
+                case 5:
+                    *val=37;
+                     break;
+            
+            default:
+                *val = 42069;
+                break;
+            }
+            
+            return IIO_VAL_INT;
+        }
+        default:
+            return -EINVAL;
+    }
+    return -EINVAL;
+}
+
+static const struct iio_info ad5592rs_info = {
+    .read_raw = &iio_ad5592rs_read_raw,
+};
+
 
 static int ad5592rs_probe ( struct spi_device *spi ) {
 
@@ -23,6 +108,8 @@ static int ad5592rs_probe ( struct spi_device *spi ) {
 ;
     indio_dev -> name = "ad5592rs";
     indio_dev -> info = &ad5592rs_info;
+    indio_dev -> channels = iio_ad5592rs_chans;
+    indio_dev -> num_channels = ARRAY_SIZE(iio_ad5592rs_chans);
 
     return devm_iio_device_register(&spi->dev, indio_dev);
  
