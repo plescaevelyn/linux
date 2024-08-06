4908529d18762907704959fc2dde9fdcf739f147
hadarauelena24 <analog@DESKTOP-SCUQ0E3.>
2024-08-06 12:38:18 +0300

    write channels
    Signed-off-by: hadarauelena24 <analog@DESKTOP-SCUQ0E3.>

--- drivers/iio/adc/iio-adc-emu.c
+++ drivers/iio/adc/iio-adc-emu.c
@@ -7,20 +7,57 @@
 #include <linux/module.h>
 #include <linux/iio/iio.h>
 
+struct iio_adc_emu_state {
+    bool en;
+    int chan0;
+    int chan1;
+};
+
 struct iio_chan_spec const iio_adc_emu_chans[]={
     {
         .type = IIO_VOLTAGE,
         .indexed = 1, //daca vreau sa numerotez canalele
         .channel = 0, //numar canal
         .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+        .info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE),
+        
     },
     {
         .type = IIO_VOLTAGE,
         .indexed = 1, //daca vreau sa numerotez canalele
         .channel = 1, //numar canal
         .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+        .info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE),
     }
 };
+
+static int iio_adc_emu_write_raw(struct iio_dev *indio_dev,
+			struct iio_chan_spec const *chan,
+			int val,
+			int val2,
+			long mask)
+{
+    struct iio_adc_emu_state *st = iio_priv(indio_dev);
+    switch(mask){
+        case IIO_CHAN_INFO_RAW:
+            if(st->en){
+                if(chan->channel)
+                    st->chan1=val;
+                else
+                    st->chan0=val;
+                return 0;
+            }
+            else
+                return -EINVAL;
+        case IIO_CHAN_INFO_ENABLE:
+            st->en = val;
+            return 0;
+        default:
+            return -EINVAL;
+    }
+
+    return -EINVAL;  
+}
 
 static int iio_adc_emu_read_raw(struct iio_dev *indio_dev,
 			struct iio_chan_spec const *chan,
@@ -28,14 +65,21 @@ static int iio_adc_emu_read_raw(struct iio_dev *indio_dev,
 			int *val2,
 			long mask)
 {
+    struct iio_adc_emu_state *st = iio_priv(indio_dev);
     switch(mask){
         case IIO_CHAN_INFO_RAW:
-            if(chan->channel)
-                *val=87;
+            if(st->en){
+                if(chan->channel)
+                    *val=st->chan1;
+                else
+                    *val=st->chan0;
+                return IIO_VAL_INT;
+            }
             else
-                *val=420;
+                return -EINVAL;
+        case IIO_CHAN_INFO_ENABLE:
+            *val=st->en;
             return IIO_VAL_INT;
-        
         default:
             return -EINVAL;
     }
@@ -45,14 +89,16 @@ static int iio_adc_emu_read_raw(struct iio_dev *indio_dev,
 
 static const struct iio_info iio_adc_emu_info = {
     .read_raw=&iio_adc_emu_read_raw,
+    .write_raw=&iio_adc_emu_write_raw,
 };
 
 static int iio_adc_emu_probe(struct spi_device *spi)
 {
     struct iio_dev *indio_dev;
+    struct iio_adc_emu_state *st;
     // int ret;
 
-    indio_dev = devm_iio_device_alloc(&spi->dev,0);
+    indio_dev = devm_iio_device_alloc(&spi->dev,sizeof(*st));
     if(!indio_dev)
         return -ENOMEM; //cod de eroare out of memory
 
@@ -61,8 +107,13 @@ static int iio_adc_emu_probe(struct spi_device *spi)
     indio_dev->channels = iio_adc_emu_chans;
     indio_dev->num_channels = ARRAY_SIZE(iio_adc_emu_chans);
 
+    st = iio_priv(indio_dev);
+    st->en = 0;
+    st->chan0=0;
+    st->chan1=0;
+
     return devm_iio_device_register(&spi->dev,indio_dev);
-};
+}
 
 static struct spi_driver iio_adc_emu_driver = {
     .driver = {
