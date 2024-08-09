f4e566ede7e602e2a66f2b7ace5bfc1ebb9a5ceb
hadarauelena24 <analog@DESKTOP-SCUQ0E3.>
2024-08-09 17:27:34 +0300

    spi transfer
    Signed-off-by: hadarauelena24 <analog@DESKTOP-SCUQ0E3.>

--- drivers/iio/adc/ad5592r_s.c
+++ drivers/iio/adc/ad5592r_s.c
@@ -3,14 +3,23 @@
  * Copyright (C) 2024 Analog Devices, Inc.
  */
 
+#include <asm/unaligned.h>
 #include <linux/spi/spi.h>
 #include <linux/module.h>
 #include <linux/iio/iio.h>
+#include <linux/bitfield.h>
+
+#define AD5592R_S_WR_ADDR_MSK GENMASK(14,11)
+#define AD5592R_S_WR_DATA_MSK GENMASK(8,0)
+#define AD5592R_S_RDB_REG_SEL GENMASK(5,2)
+#define AD5592R_S_RDB_EN BIT(6)
+
+#define AD5592R_S_CONF_RDB_REG 0X7
 
 struct ad5592r_s_state {
+    struct spi_device *spi;
     bool en;
-    int chan0;
-    int chan1;
+    int chan[6];
 };
 
 struct iio_chan_spec const ad5592r_s_chans[]={
@@ -27,6 +36,34 @@ struct iio_chan_spec const ad5592r_s_chans[]={
         .channel = 1, //numar canal
         .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
         .info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE),
+    },
+    {
+        .type = IIO_VOLTAGE,
+        .indexed = 1, //daca vreau sa numerotez canalele
+        .channel = 2, //numar canal
+        .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+        .info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE),
+    },
+    {
+        .type = IIO_VOLTAGE,
+        .indexed = 1, //daca vreau sa numerotez canalele
+        .channel = 3, //numar canal
+        .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+        .info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE),
+    },
+    {
+        .type = IIO_VOLTAGE,
+        .indexed = 1, //daca vreau sa numerotez canalele
+        .channel = 4, //numar canal
+        .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+        .info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE),
+    },
+    {
+        .type = IIO_VOLTAGE,
+        .indexed = 1, //daca vreau sa numerotez canalele
+        .channel = 5, //numar canal
+        .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+        .info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE),
     }
 };
 
@@ -40,10 +77,7 @@ static int ad5592r_s_read_raw(struct iio_dev *indio_dev,
     switch(mask){
         case IIO_CHAN_INFO_RAW:
             if(st->en){
-                if(chan->channel)
-                    *val=st->chan1;
-                else
-                    *val=st->chan0;
+                *val=st->chan[chan->channel];
                 return IIO_VAL_INT;
             }
             else
@@ -68,10 +102,7 @@ static int ad5592r_s_write_raw(struct iio_dev *indio_dev,
     switch(mask){
         case IIO_CHAN_INFO_RAW:
             if(st->en){
-                if(chan->channel)
-                    st->chan1=val;
-                else
-                    st->chan0=val;
+                st->chan[chan->channel]=val;
                 return 0;
             }
             else
@@ -86,9 +117,104 @@ static int ad5592r_s_write_raw(struct iio_dev *indio_dev,
     return -EINVAL;  
 }
 
+static int ad5592r_s_spi_write(struct ad5592r_s_state *st, u8 reg, u16 writeval)
+{
+    u16 tx = 0;
+    u16 msg = 0;
+
+    struct spi_transfer xfer[]={
+        {
+            .tx_buf=&msg,
+            .rx_buf=NULL,
+            .len=2,
+        }
+    };
+
+    tx = FIELD_PREP(AD5592R_S_WR_ADDR_MSK,reg) | FIELD_PREP(AD5592R_S_WR_DATA_MSK,writeval);
+
+    dev_info(&st->spi->dev, "tx = 0x%X", tx);
+
+    put_unaligned_be16(tx,&msg);
+    dev_info(&st->spi->dev, "msg = 0x%X", msg);
+
+    return spi_sync_transfer(st->spi,xfer,1);
+}
+
+static int ad5592r_s_spi_nop(struct ad5592r_s_state *st, u16 *val)
+{
+    u16 tx = 0;
+    struct spi_transfer xfer[] = {
+        {
+            .tx_buf = &tx,
+            .rx_buf = val,
+            .len = 2,
+        }
+    };
+
+    return spi_sync_transfer(st->spi,xfer,1);
+}
+
+static int ad5592r_s_spi_read(struct ad5592r_s_state *st, u8 reg, u16 *readval)
+{
+    u16 tx = 0;
+    u16 rx = 0;
+    int ret;
+    u16 msg = 0;
+
+    struct spi_transfer xfer[] = {
+        {
+            .rx_buf = NULL,
+            .tx_buf = &msg,
+            .len = 2,
+        }
+    };
+    
+    tx |= FIELD_PREP(AD5592R_S_WR_ADDR_MSK,AD5592R_S_CONF_RDB_REG);
+    tx |= AD5592R_S_RDB_EN;
+    tx |= FIELD_PREP(AD5592R_S_RDB_REG_SEL, reg);
+
+    dev_info(&st->spi->dev, "tx = 0x%X", tx);
+
+    put_unaligned_be16(tx,&msg);
+
+    dev_info(&st->spi->dev, "msg = 0x%X", msg);
+    
+    ret = spi_sync_transfer(st->spi,xfer,1);
+    if(ret){
+        dev_err(&st->spi->dev, "Failed spi transfer config reg");
+        return ret;
+    }
+
+    ret = ad5592r_s_spi_nop(st, &rx);
+
+    if(ret){
+        dev_err(&st->spi->dev, "Failed nop transfer");
+        return ret;
+    }
+
+    *readval = get_unaligned_be16(&rx);
+
+
+    return 0;
+
+}
+
+static int ad5592r_s_debugfs(struct iio_dev *indio_dev,
+				  unsigned reg, unsigned writeval,
+				  unsigned *readval)
+{
+    struct ad5592r_s_state *st = iio_priv(indio_dev);
+
+    if(readval)
+        return ad5592r_s_spi_read(st,reg,(u16 *)readval);
+
+    return ad5592r_s_spi_write(st,reg,writeval);
+}
+
 static const struct iio_info ad5592r_s_info = {
      .read_raw=&ad5592r_s_read_raw,
      .write_raw=&ad5592r_s_write_raw,
+     .debugfs_reg_access = &ad5592r_s_debugfs,
 };
 
 static int ad5592r_s_probe(struct spi_device *spi)
@@ -108,8 +234,9 @@ static int ad5592r_s_probe(struct spi_device *spi)
 
     st = iio_priv(indio_dev);
     st->en = 0;
-    st->chan0=0;
-    st->chan1=0;
+    for(int i=0;i<6;i++)
+        st->chan[i]=0;
+    st->spi = spi;
 
     return devm_iio_device_register(&spi->dev,indio_dev);
 };
