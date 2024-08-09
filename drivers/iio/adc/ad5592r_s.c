a0dc853c0318485e3f910bf83dc8833276f6a816
octavianCraciun <octavian.craciun0409@gmail.com>
2024-08-09 08:15:58 -0700

    Add SPI read/write for ad5592r_s driver

--- drivers/iio/adc/ad5592r_s.c
+++ drivers/iio/adc/ad5592r_s.c
@@ -1,48 +1,31 @@
 #include <linux/module.h>
 #include <linux/spi/spi.h>
 #include <linux/iio/iio.h>
+#include <linux/bitfield.h>
+#include <asm/unaligned.h>
 
-static int ad5592r_s_probe(struct spi_device*);
-static int ad5592r_s_read_raw(struct iio_dev*, struct iio_chan_spec const*, int*, int*, long);
-static int ad5592r_s_write_raw(struct iio_dev*, struct iio_chan_spec const*, int, int, long);
+#define AD5592R_S_WR_ADDR_MASK                      GENMASK(14,11)
+#define AD5592R_S_WR_VALUE_MASK                     GENMASK(8,0)
 
-/**
- * Driver structure
- */
-static struct spi_driver ad5592r_s_driver =
-{
-    .driver = {
-        .name = "ad5592r_s",
-    },
-    .probe = ad5592r_s_probe
-};
+#define AD5592R_S_RDBK_REG_SEL_MASK                 GENMASK(5,2)
+#define AD5592R_S_RDBK_EN_MASK                      BIT(6)
 
-/**
- * Driver state
- */
+#define AD5592R_S_CONF_RDBK_REG_ADDR                0x7
+
+
 struct ad5582r_s_state {
+    struct spi_device *spi;
     bool enable;
-    int channel0;
-    int channel1;
-    int channel2;
-    int channel3;
-    int channel4;
-    int channel5;
-    int channel6;
-    int channel7;
+    u16 channel0;
+    u16 channel1;
+    u16 channel2;
+    u16 channel3;
+    u16 channel4;
+    u16 channel5;
+    u16 channel6;
+    u16 channel7;
 };
 
-/**
- * IIO_INFO structure
- */
-static const struct iio_info ad5592r_s_info = {
-    .read_raw = &ad5592r_s_read_raw,
-    .write_raw = &ad5592r_s_write_raw,
-};
-
-/**
- *  Channels specification structure 
- */
 static const struct iio_chan_spec ad5592r_s_channels[] = {
     {
         .type = IIO_VOLTAGE,
@@ -95,18 +78,6 @@ static const struct iio_chan_spec ad5592r_s_channels[] = {
     }
 };
 
-/**
- * Function to read the raw values of the adc5592r_s channels
- * indio_dev    - the created iio-adi-emu device
- * chan         - the channels struct
- * val_whole    - the whole part of the returned value
- * val_frac     - the fractional part of the returned value
- * mask         - the operation which will be performed
- * 
- * returns  IIO_VAL_INT and sets val to the enable value when mask is IIO_CHAN_INFO_ENABLE
- * returns   IIO_VAL_INT and sets the val to the channel number when mask is IIO_CHAN_INFO_RAW
- * returns   -EINVAL when the mask has unexpected value
- */
 static int ad5592r_s_read_raw(struct iio_dev* indio_dev,
     struct iio_chan_spec const *chan,
     int *val_whole,
@@ -155,17 +126,6 @@ static int ad5592r_s_read_raw(struct iio_dev* indio_dev,
         }
 }
 
-/**
- * Function to write a raw value to the ad5592r_s channels
- * indio_dev    - the created iio-adi-emu device
- * chan         - the channels struct
- * val          - the whole part of the raw value
- * val1         - the fractional part of the raw value
- * mask         - the operation which will be performed
- * 
- * return 0 when the mask is IIO_CHAN_INFO_ENABLED
- * return -EINVAL when the mask has unexpected value
- */
 static int ad5592r_s_write_raw(struct iio_dev* indio_dev,
     struct iio_chan_spec const *chan,
     int val_whole,
@@ -206,26 +166,120 @@ static int ad5592r_s_write_raw(struct iio_dev* indio_dev,
                 }
                 return 0;
             default:
-                // Error: Invalid value for mask
                 return -EINVAL;
         }
 }
 
-/**
- * Probe function executed when the devicetree is parsed
- * parameter spi - the parent spi controller
- * returns 0 on success or negative number on error  
- */
+
+static int ad5592r_s_spi_noop(struct ad5582r_s_state *state, u16 *ret_val) {
+    u16 tx = 0;
+    struct  spi_transfer xfer[] = {
+        {
+            .rx_buf = ret_val,
+            .tx_buf = &tx,
+            .len = 2,
+        }
+    };
+    return spi_sync_transfer(state->spi, xfer, 1);
+} 
+
+static int ad5592r_s_spi_read(struct ad5582r_s_state *state,
+    u8 reg_addr,
+    u16 *read_val) {
+        u16 tx = 0;
+        u16 rx = 0;
+        u16 msg = 0;
+        struct  spi_transfer xfer[] = {
+            {
+                .rx_buf = NULL,
+                .tx_buf = &tx,
+                .len = 2,
+            },
+        };
+
+        msg |= FIELD_PREP(AD5592R_S_WR_ADDR_MASK, AD5592R_S_CONF_RDBK_REG_ADDR);
+        msg |= AD5592R_S_RDBK_EN_MASK;
+        msg |= FIELD_PREP(AD5592R_S_RDBK_REG_SEL_MASK, reg_addr);
+        put_unaligned_be16(msg, &tx);
+        dev_info(&state->spi->dev, "SPI read msg: %X", msg);
+        dev_info(&state->spi->dev, "SPI read tx %X", tx);
+
+        int ret = spi_sync_transfer(state->spi, xfer, 1);
+        if (ret) {
+            dev_err(&state->spi->dev, "SPI transfer failed on config reg");
+            return ret;
+        }
+
+        ret = ad5592r_s_spi_noop(state, &rx);
+        if (ret) {
+            dev_err(&state->spi->dev, "SPI sync transfer failed on noop");
+            return ret;
+        }
+        *read_val = get_unaligned_be16(&rx);
+        dev_info(&state->spi->dev, "SPI read rx %X", rx);
+        dev_info(&state->spi->dev, "SPI read read_val %X", *read_val);
+
+        return 0;
+}
+
+static int ad5592r_s_spi_write(struct ad5582r_s_state *state,
+    u8 reg_addr,
+    u16 write_val) {
+        u16 tx = 0;
+        u16 msg = 0;
+        struct spi_transfer xfer[] = {
+            {
+                .tx_buf = &tx,
+                .rx_buf = NULL,
+                .len = 2,
+            },
+        };
+
+        msg |= FIELD_PREP(AD5592R_S_WR_ADDR_MASK, reg_addr);
+        msg |= FIELD_PREP(AD5592R_S_WR_VALUE_MASK, write_val);
+        put_unaligned_be16(msg, &tx);
+        dev_info(&state->spi->dev, "SPI write msg: %X", msg);
+        dev_info(&state->spi->dev, "SPI write tx: %X", tx);
+
+        int ret = spi_sync_transfer(state->spi, xfer, 1);
+        if (ret) {
+            dev_err(&state->spi->dev, "SPI sync transfer failed on write");
+            return ret;
+        }
+
+        return 0;
+    }
+
+static int ad5592r_s_debugfx(struct iio_dev *indio_dev,
+    unsigned reg_addr,
+    unsigned write_val,
+    unsigned *read_val) {
+        struct ad5582r_s_state *state = iio_priv(indio_dev);
+
+        if (read_val) {
+            return ad5592r_s_spi_read(state, reg_addr, (u16 *)read_val);
+        } else {
+            return ad5592r_s_spi_write(state, reg_addr, write_val);
+        }
+}
+
+static const struct iio_info ad5592r_s_info = {
+    .read_raw = &ad5592r_s_read_raw,
+    .write_raw = &ad5592r_s_write_raw,
+    .debugfs_reg_access = &ad5592r_s_debugfx,
+};
+
 static int ad5592r_s_probe(struct spi_device *spi) {
     struct iio_dev *indio_dev;
     struct ad5582r_s_state *state;
 
     indio_dev = devm_iio_device_alloc(&spi->dev, sizeof(*state));
     if (!indio_dev) {
-        return -ENOMEM; // Error: Not enogh memory!
+        return -ENOMEM;
     }
 
     state = iio_priv(indio_dev);
+    state->spi = spi;
     state->enable = 0;
     state->channel0 = 0;
     state->channel1 = 0;
@@ -243,6 +297,14 @@ static int ad5592r_s_probe(struct spi_device *spi) {
 
     return devm_iio_device_register(&spi->dev, indio_dev);
 }
+
+static struct spi_driver ad5592r_s_driver =
+{
+    .driver = {
+        .name = "ad5592r_s",
+    },
+    .probe = ad5592r_s_probe
+};
 
 module_spi_driver(ad5592r_s_driver);
 
