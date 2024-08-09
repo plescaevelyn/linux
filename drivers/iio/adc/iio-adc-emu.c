3babd404f34027623d8c11563e9a175107670efb
Wally4Ever <andrei_valentyn_2008@yahoo.com>
2024-08-09 14:48:25 +0300

    SPI wr/rd EMU, v1.3

--- drivers/iio/adc/iio-adc-emu.c
+++ drivers/iio/adc/iio-adc-emu.c
@@ -7,12 +7,19 @@
 #include <linux/spi/spi.h>
 #include <linux/module.h>
 #include <linux/iio/iio.h>
+#include <linux/bitfield.h>         
+
+#define IIO_ADC_ADDR_RD_MSK         GENMASK(6,0)
+#define IIO_ADC_EMU_RD_MSK          BIT(7)
+#define IIO_ADC_ADDR_WR_MSK         GENMASK(14,8)
+#define IIO_ADC_VAL_MSK             GENMASK(7,0)
 
 struct iio_adc_emu_state {
     bool en;
     int chan0, chan1;
+    struct spi_device *spi;
 };  
-
+/////////////////////////////////////////////////
 struct iio_chan_spec const iio_adc_emu_chans[] = {
     {
         .type = IIO_VOLTAGE,
@@ -30,7 +37,7 @@ struct iio_chan_spec const iio_adc_emu_chans[] = {
     }
 
 };
-
+///////////////////////////////////////////////
 static int iio_adc_emu_read_raw  (struct iio_dev *indio_dev,
 			struct iio_chan_spec const *chan,
 			int *val,
@@ -73,27 +80,81 @@ static int iio_adc_emu_write_raw (struct iio_dev *indio_dev,
 		    st->en = val;
 		    return 0;
         case IIO_CHAN_INFO_RAW:
-        if (st->en){
-            if(chan->channel)
-                st->chan1 = val;
+            if (st->en){
+                if(chan->channel)
+                    st->chan1 = val;
+                else
+                    st->chan0 = val;
+                return 0;
+            } 
             else
-                st->chan0 = val;
-            return 0;
-        }
-        else
-            return -EINVAL;
+                return -EINVAL;
 	    default:
 		    return -EINVAL;
 	}
 }
+////////////////////////////////////////////////////////////
+static int iio_adc_emu_spi_read(struct iio_adc_emu_state *st, u8 reg, u8 *readval)
+{
+    u8 tx, rx;
+    int ret;
+    struct spi_transfer xfer[] = {
+        {
+            .rx_buf = NULL,
+            .tx_buf = &tx,
+            .len = 1,
+        },
+        {
+            .rx_buf = &rx,
+            .tx_buf = NULL,
+            .len = 1,
+        }
+    };
+    tx = IIO_ADC_EMU_RD_MSK | FIELD_PREP(IIO_ADC_ADDR_RD_MSK, reg);
+    ret = spi_sync_transfer(st->spi, xfer, 2);
+    if(ret) {
+        dev_err(&st->spi->dev, "Spi sync transfer fail during read");
+        return ret;
+    }
+    *readval = rx;
+    return 0;
+}
+/////////////////////////////////////////////////////////////////
+static int iio_adc_emu_spi_write (struct iio_adc_emu_state *st, u8 reg, u8 writeval)
+{
+        u16 tx = 0;
+        u16 msg;
+        struct spi_transfer xfer = {
+            .rx_buf =   NULL,
+            .tx_buf = &msg,
+            .len = 2,
+                
+        };
 
+        tx = FIELD_PREP(IIO_ADC_ADDR_WR_MSK, reg) | FIELD_PREP (IIO_ADC_VAL_MSK, writeval);
+        dev_info(&st->spi->dev, "tx = 0x%X", tx);
+        put_unaligned_be16(tx, &msg);
+         dev_info(&st->spi->dev, "msg = 0x%X", msg);
+        return spi_sync_transfer(st->spi, &xfer, 1);
+}
+/////////////////////////////////////////////////////////////////
+static int iio_adc_emu_debugfs (struct iio_dev *indio_dev, unsigned reg, unsigned writeval, unsigned *readval)
+{
+    struct iio_adc_emu_state *st = iio_priv(indio_dev);
+    if(readval)
+        return iio_adc_emu_spi_read(st, reg, (u8*)readval);
 
+    return iio_adc_emu_spi_write(st, reg, writeval);
+}
+///////////////////////////////////////////////////////////
 static const struct iio_info iio_adc_emu_info = {
     .read_raw = &iio_adc_emu_read_raw,
     .write_raw = &iio_adc_emu_write_raw,
+    .debugfs_reg_access = &iio_adc_emu_debugfs,
 };
-////////////////////////////////////////////////////////////
-static int iio_adc_emu_probe ( struct spi_device *spi ) {
+/////////////////////////////////////////////////////////////////
+static int iio_adc_emu_probe ( struct spi_device *spi ) 
+{
 
     struct iio_dev *indio_dev;
     struct iio_adc_emu_state *st;
@@ -112,8 +173,8 @@ static int iio_adc_emu_probe ( struct spi_device *spi ) {
     indio_dev -> num_channels = ARRAY_SIZE(iio_adc_emu_chans);
 
     st = iio_priv (indio_dev);
-    st -> en = 0;
-
+    st -> en = 0; 
+    st -> spi = spi;
     return devm_iio_device_register(&spi->dev, indio_dev);
  
 }
