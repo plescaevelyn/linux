3c8235a82c3c1cb7d48a672b305052d213fa533e
florina2002 <nechitaflorina2002@gmail.com>
2024-08-19 10:18:57 +0300

    //dev_info
    Signed-off-by: florina2002 <nechitaflorina2002@gmail.com>

---
+++ control_leds_adc.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+while true
+do
+#x
+cat /sys/bus/iio/devices/iio\:device0/in_voltage0_raw > /sys/bus/iio/devices/iio\:device2/out_count0_raw
+cat /sys/bus/iio/devices/iio\:device0/in_voltage1_raw > /sys/bus/iio/devices/iio\:device2/out_count3_raw
+#y
+cat /sys/bus/iio/devices/iio\:device0/in_voltage2_raw > /sys/bus/iio/devices/iio\:device2/out_count1_raw
+cat /sys/bus/iio/devices/iio\:device0/in_voltage3_raw > /sys/bus/iio/devices/iio\:device2/out_count4_raw
+#z
+cat /sys/bus/iio/devices/iio\:device0/in_voltage4_raw > /sys/bus/iio/devices/iio\:device2/out_count2_raw
+cat /sys/bus/iio/devices/iio\:device0/in_voltage5_raw > /sys/bus/iio/devices/iio\:device2/out_count5_raw
+done
