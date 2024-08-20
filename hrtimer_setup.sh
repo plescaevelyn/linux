88e980f7f20046c6d0522b316812bbc350ef4f60
Raluca Zaicanu <ralucazaicanu@yahoo.ro>
2024-08-20 12:10:39 +0300

    Timer script
    Signed-off-by: Raluca Zaicanu <ralucazaicanu@yahoo.ro>

---
+++ hrtimer_setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+ mkdir configfs
+ mount -t configfs none configfs
+ systemctl daemon-reload
+ mkdir configfs/iio/triggers/hrtimer/tmr0
+ echo 1 > /sys/bus/iio/devices/iio\:device0/en
+ echo tmr0 > /sys/bus/iio/devices/iio\:device0/trigger/current_trigger
+
+ echo 1000 > /sys/bus/iio/devices/trigger0/sampling_frequency
